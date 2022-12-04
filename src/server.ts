0875382ac1608dae0a622fd66fed1382cde22132
Silvio Dayube <scarige@gmail.com>
2022-12-04 11:55:55 -0300

    Add cors headers

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,12 @@ import { HttpError } from './errors/HttpError';
 
 const app = express();
 
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(router);
 app.use(
